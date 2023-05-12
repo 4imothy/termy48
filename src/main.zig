61355427ce5df2f36e54a82b5c25b2b0d69f1f92
Timothy Cronin <tcroniniv@gmail.com>
2023-05-12 10:49:18 -0400

    cleanup

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: GPL-3.0
 
-// TODO better exiting handling, error type one is not enough room to start
 // TODO generalize the input stuff for all os
-// TODO, error handling, might not need for space things, for when sizes are bad
-// Check if defer actions are done on ctrl + c
-// TODO on exit print to show the cursor
+// TODO error for when num rows is greater than height of the window
 // TODO eventually read num_rows, num_cols from arguments
 //* Styling *\\
 //* TypeName | namespace_name | global_var | functionName | const_name *\\
 const std = @import("std");
 const Board = @import("Board.zig");
+const f = @import("formats.zig");
+const errors = @import("errors.zig");
+const system = std.os.system;
 const out = std.io.getStdOut();
 var buf = std.io.bufferedWriter(out.writer());
 var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
@@ -21,29 +21,39 @@ pub const allocator = arena.allocator();
 pub fn main() !void {
     defer arena.deinit();
     // defaults
-    var num_cols: usize = 4;
-    var num_rows: usize = 4;
+    var num_cols: usize = 0;
+    var num_rows: usize = 0;
     var piece_size: u8 = 4;
-    var tty: ?std.os.fd_t = try std.os.open("/dev/tty", std.os.system.O.RDWR, 0);
+    var tty: ?std.os.fd_t = try std.os.open("/dev/tty", system.O.RDWR, 0);
     var dims: [2]usize = try getDimensions(tty);
     var screen_width = dims[0];
     var screen_height = dims[1];
+    try buf_wrtr.print(f.hide_cursor, .{});
+
+    // TODO Get the cursor position
+    // Change this in future to hopefully work inline
+    // have to save the position as when printing below and
+    // the board moves up the restored position is wrong
     const board = try Board.init(piece_size, num_rows, num_cols);
     defer board.deinit();
     var rnd = std.rand.DefaultPrng.init(@truncate(u64, @bitCast(u128, std.time.nanoTimestamp())));
     // check if there is enough space to start the game
     const t = try board.addRandomPiece(&rnd);
     if (!t) {
-        board.deinit();
-        try buf_wrtr.print("Not Enough Space to Start\n", .{});
-        try buf.flush();
-        try std.os.exit(1);
+        try exitGameOnError(errors.not_enough_space, .{});
     }
+    try buf_wrtr.print(f.clear_page, .{});
+    try buf_wrtr.print(f.set_cursor_pos, .{ 0, 0 });
     try buf_wrtr.print("W: {d}, H: {d}\n", .{ screen_width, screen_height });
     try buf.flush();
     var orig = try std.os.tcgetattr(std.os.STDIN_FILENO);
     var new = orig;
-    new.lflag &= ~(std.os.darwin.ECHO | std.os.darwin.ICANON);
+    // TODO try this on windows, don't think it will work
+    // make it it's own branch
+    // ISIG: Disable vanilla CTRL-C and CTRL-Z
+    // ECHO: Stop the terminal from displaying pressed keys.
+    // ICANON: Allows us to read inputs byte-wise instead of line-wise.
+    new.lflag &= ~(system.ECHO | system.ICANON | system.ISIG);
     try std.os.tcsetattr(std.os.STDIN_FILENO, std.os.TCSA.FLUSH, new);
     defer std.os.tcsetattr(std.os.STDIN_FILENO, std.os.TCSA.FLUSH, orig) catch {};
     var char: u8 = undefined;
@@ -52,11 +62,16 @@ pub fn main() !void {
     var running = true;
     while (running) {
         char = try reader.readByte();
-        std.debug.print("{}\n", .{char});
         switch (char) {
             'q' => {
-                std.debug.print("q pressed\n", .{});
-                try std.os.exit(0);
+                try exitGame();
+            },
+            'c' & '\x1F' => {
+                try exitGame();
+            },
+            'h' => {
+                try board.draw();
+                try buf.flush();
             },
             else => {},
         }
@@ -67,10 +82,22 @@ fn getDimensions(tty: ?std.os.fd_t) ![2]usize {
     if (tty == null) {
         return .{ 100, 100 };
     }
-    var size = std.mem.zeroes(std.os.system.winsize);
-    const err = std.os.system.ioctl(tty.?, std.os.system.T.IOCGWINSZ, @ptrToInt(&size));
+    var size = std.mem.zeroes(system.winsize);
+    const err = system.ioctl(tty.?, system.T.IOCGWINSZ, @ptrToInt(&size));
     if (std.os.errno(err) != .SUCCESS) {
-        return std.os.unexpectedErrno(@intToEnum(std.os.system.E, err));
+        return std.os.unexpectedErrno(@intToEnum(system.E, err));
     }
     return .{ size.ws_col, size.ws_row };
+}
+
+fn exitGame() !void {
+    try buf_wrtr.print(f.show_cursor, .{});
+    try buf.flush();
+    std.os.exit(0);
+}
+
+pub fn exitGameOnError(comptime format: []const u8, args: anytype) !void {
+    try buf_wrtr.print(format, args);
+    try buf.flush();
+    std.os.exit(1);
 }
