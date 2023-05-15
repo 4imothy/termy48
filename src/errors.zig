a7acb42b8508b1bb55c819c7968db979f9cbd47a
Timothy Cronin <tcroniniv@gmail.com>
2023-05-14 21:02:36 -0400

    add newline

--- src/errors.zig
+++ src/errors.zig
@@ -1,7 +1,7 @@
 pub const insuf_space_for_numbers: []const u8 = "Board is Too Small for numbers to fit\n";
 pub const insuf_space_for_board: []const u8 =
     \\ Board is too large for your screen
-    \\ Maybe make the font smaller?"
+    \\ Maybe make the font smaller?\n"
 ;
 pub const no_equals_setting_arg: []const u8 = "No assignment to an argument which needs one. Argument given: {s}\n";
 pub const unable_to_parse: []const u8 = "Unable to parse |{s}| to |{}| in argument |{s}|\n";
