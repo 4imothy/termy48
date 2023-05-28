4436f89f25b6921e3be4682c4ef6038e21d34870
Timothy Cronin <tcroniniv@gmail.com>
2023-05-27 23:14:22 -0400

    release safe as default

--- build.zig
+++ build.zig
@@ -13,7 +13,7 @@ pub fn build(b: *std.build.Builder) void {
 
     const exe = b.addExecutable("termy48", "src/main.zig");
     exe.setTarget(target);
-    exe.setBuildMode(mode);
+    exe.setBuildMode(.ReleaseSafe);
     exe.install();
 
     const run_cmd = exe.run();
